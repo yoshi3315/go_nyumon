a0201a9a8a2e4a14d40976485c4ac7e3c6c83f96
yoshiki chiba <yoshiki.chiba@one-star.jp>
2020-06-07 15:28:09 +0900

    section13.3:勝敗と引き分けの判定

--- chapter13/game.go
+++ chapter13/game.go
@@ -11,8 +11,11 @@ const maru, batsu = "○", "✕"
 type Board [3][3]string
 
 type ViewData struct {
-	Turn  string
-	Board *Board
+	Turn   string
+	Board  *Board
+	Win    bool
+	Draw   bool
+	Winner string
 }
 
 var tmpl *template.Template = template.Must(template.ParseFiles("game.tmpl"))
@@ -25,11 +28,67 @@ func (v *ViewData) Execute(w http.ResponseWriter) {
 }
 
 func gameHandle(w http.ResponseWriter, r *http.Request) {
-	_, nextTurn := turnFormValue(r)
+	turn, nextTurn := turnFormValue(r)
 	board := boardFormValue(r)
 
-	v := ViewData{nextTurn, board}
+	win, draw, winner := false, false, ""
+
+	if turn != "" {
+		win = board.win(turn)
+
+		if win {
+			winner = turn
+			board.setBar()
+		} else {
+			draw = board.draw()
+		}
+	}
+
+	v := ViewData{nextTurn, board, win, draw, winner}
 	v.Execute(w)
+}
+
+var winBoardIndexArray = [...][3]struct{ row, col int }{
+	{{0, 0}, {0, 1}, {0, 2}},
+	{{1, 0}, {1, 1}, {1, 2}},
+	{{2, 0}, {2, 1}, {2, 2}},
+
+	{{0, 0}, {1, 0}, {2, 0}},
+	{{0, 1}, {1, 1}, {2, 1}},
+	{{0, 2}, {1, 2}, {2, 2}},
+
+	{{0, 0}, {1, 1}, {2, 2}},
+	{{0, 2}, {1, 1}, {2, 0}},
+}
+
+func (b *Board) win(turn string) bool {
+	for _, w := range winBoardIndexArray {
+		if (b[w[0].row][w[0].col] == turn) && (b[w[1].row][w[1].col] == turn) && (b[w[2].row][w[2].col] == turn) {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *Board) draw() bool {
+	for row, rows := range b {
+		for col, _ := range rows {
+			if b[row][col] == "" {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func (b *Board) setBar() {
+	for row, rows := range b {
+		for col, _ := range rows {
+			if b[row][col] == "" {
+				b[row][col] = "-"
+			}
+		}
+	}
 }
 
 func boardFormValue(r *http.Request) *Board {
