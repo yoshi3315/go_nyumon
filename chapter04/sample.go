32f5c7ce833a1e9512d5eccf5277b4642c6f072d
yoshiki chiba <yoshiki.chiba@one-star.jp>
2020-06-14 20:54:48 +0900

    section4.5:定数

--- chapter04/sample.go
+++ chapter04/sample.go
@@ -46,6 +46,41 @@ var (
 	s4 = "¥xE3¥x81¥x82"
 )
 
+const (
+	c1 int = 1
+	c2     = c1
+	c3     = 1
+)
+
+var (
+	v1 int = c3
+	v2 int = c3
+)
+
+const (
+	u1 int = 1
+	u2
+	u3
+)
+
+const (
+	a0     = iota
+	a1     = iota
+	a2     = 9
+	a3     = iota
+	a4, a5 = iota, iota
+	a6     = iota * iota
+)
+
+const (
+	cc0 = 1 << iota
+	cc1
+	cc2
+)
+
+const d0 = iota
+const d1 = iota
+
 func main() {
 	x3 := 1
 	y3, z3 := 2, 3
@@ -67,6 +102,12 @@ func main() {
 	fmt.Println(n1, n2, n3, f1, f2, i2, i3)
 	fmt.Println(r1, r2, b1, b2)
 	fmt.Println(s1, s2, s3, s4)
+
+	fmt.Println(c1, c2, c3, v1, v2, u1, u2, u3)
+
+	fmt.Println(a0, a1, a2, a3, a4, a5, a6)
+	fmt.Println(cc0, cc1, cc2)
+	fmt.Println(d0, d1)
 }
 
 func f() {
