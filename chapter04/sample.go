0181800c9b449334f8ce1e08310d1764b8a4c173
yoshiki chiba <yoshiki.chiba@one-star.jp>
2020-06-14 20:03:29 +0900

    section4.3:変数のスコープ

--- chapter04/sample.go
+++ chapter04/sample.go
@@ -18,10 +18,27 @@ var z1, z2 = 1, "a"
 var i1, err1 = strconv.Atoi("10")
 var _, err2 = strconv.Atoi("10")
 
+var n = 1
+
 func main() {
 	x3 := 1
 	y3, z3 := 2, 3
 
 	fmt.Println(x, y, z)
 	fmt.Println(x3, y3, z3)
+
+	fmt.Println(n)
+	f()
+	fmt.Println(n)
+
+	p := 1
+
+	if p == 1 {
+		q := 2
+		fmt.Println(p, q)
+	}
+}
+
+func f() {
+	n = 2
 }
