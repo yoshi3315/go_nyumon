cc86e102830063a2805e271d7235b9d2ac4b6a3b
yoshiki chiba <yoshiki.chiba@one-star.jp>
2020-06-14 21:35:34 +0900

    section5.2:複素数の操作

--- chapter05/sample.go
+++ chapter05/sample.go
@@ -54,6 +54,29 @@ func main() {
 
 	i--
 	fmt.Println(i)
+
+	var (
+		f32r float32 = 1.1
+		f32i float32 = 2.2
+		f64r float64 = 1.1
+		f64i float64 = 2.2
+	)
+
+	var c64 complex64 = complex(f32r, f32i)
+	fmt.Println("c64: ", c64)
+
+	var c128 complex128 = complex(f64r, f64i)
+	fmt.Println("c128: ", c128)
+
+	f32r = real(c64)
+	f32i = imag(c64)
+	fmt.Println("f32r: ", f32r)
+	fmt.Println("f32i: ", f32i)
+
+	f64r = real(c128)
+	f64i = imag(c128)
+	fmt.Println("f64r: ", f64r)
+	fmt.Println("f64i: ", f64i)
 }
 
 func trueFunc() bool {
