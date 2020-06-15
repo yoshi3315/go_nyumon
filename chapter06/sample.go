d8c78512f17af9f45172f6832380f62b7cba1112
yoshiki chiba <yoshiki.chiba@one-star.jp>
2020-06-15 20:30:10 +0900

    section6.2:switch 文

--- chapter06/sample.go
+++ chapter06/sample.go
@@ -10,4 +10,34 @@ func main() {
 	} else {
 		fmt.Println(x)
 	}
+
+	x := 1
+	switch y := x + 1; y {
+	case 1:
+		z := x + y
+		fmt.Println("case 1:", x, y, z)
+	case 2:
+		fmt.Println("case 2:", x, y)
+	}
+
+	switch x {
+	case 1:
+		fmt.Println("A")
+		fallthrough
+	case 2:
+		fmt.Println("B")
+	case 3:
+		fmt.Println("C")
+	}
+
+	y := 2
+	switch x {
+	case 1:
+		fmt.Println("A")
+		if y == 2 {
+			break
+		}
+
+		fmt.Println("B")
+	}
 }
