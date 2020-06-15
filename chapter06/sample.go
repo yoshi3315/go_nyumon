1df7403e5c295521eedf2d596dc79c9029e2ba45
yoshiki chiba <yoshiki.chiba@one-star.jp>
2020-06-15 20:43:51 +0900

    section6.3:for 文

--- chapter06/sample.go
+++ chapter06/sample.go
@@ -40,4 +40,67 @@ func main() {
 
 		fmt.Println("B")
 	}
+
+	n := 0
+	for i := 1; i <= 10; i++ {
+		n += i
+	}
+	fmt.Println(n)
+
+	for i := 1; i <= 10; i++ {
+		if (i % 2) == 0 {
+			continue
+		}
+
+		fmt.Println(i, ",")
+	}
+
+	fmt.Println()
+
+	for i := 1; i <= 10; i++ {
+		if i == 5 {
+			break
+		}
+		fmt.Print(i, ",")
+	}
+
+OuterLoop:
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			fmt.Println(i, j)
+
+			if (i == 0) && (j == 1) {
+				fmt.Println("continue")
+				continue OuterLoop
+			}
+
+			if (i == 1) && (j == 1) {
+				fmt.Println("break")
+				break OuterLoop
+			}
+		}
+	}
+
+	m := 1
+	for m <= 1000 {
+		m *= 2
+	}
+
+	fmt.Println(m)
+
+	o := 1
+	for {
+		o *= 2
+		if 1000 <= o {
+			break
+		}
+	}
+
+	fmt.Println(o)
+
+	s := "aあいう"
+
+	for i, r := range s {
+		fmt.Println(i, r, string(r))
+	}
 }
